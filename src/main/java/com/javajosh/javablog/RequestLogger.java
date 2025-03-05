fa268d71c99a1bd7d2bcec41bb69a50bacca3dce
javajosh <javajosh@gmail.com>
2025-03-04 21:09:52 -0500

    Add a simple controller, a way to call it, and a component that logs requests.

---
+++ src/main/java/com/javajosh/javablog/RequestLogger.java
@@ -0,0 +1,31 @@
+package com.javajosh.javablog;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+@Component
+public class RequestLogger implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+    String requestLog = String.format("Method: %s, Path: %s, Client IP: %s",
+        httpRequest.getMethod(),
+        httpRequest.getRequestURI(),
+        httpRequest.getRemoteAddr());
+
+    System.out.println("Incoming Request: " + requestLog);
+
+    chain.doFilter(request, response);
+  }
+}
