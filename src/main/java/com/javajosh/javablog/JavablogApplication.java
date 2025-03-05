e92a2f8349edd82ff00813a5f201a6e06fc34398
javajosh <javajosh@gmail.com>
2025-03-04 19:48:45 -0500

    Initial commit - does not run because of missing database.

---
+++ src/main/java/com/javajosh/javablog/JavablogApplication.java
@@ -0,0 +1,13 @@
+package com.javajosh.javablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavablogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavablogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javajosh/javablog/JavablogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javajosh.javablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavablogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
