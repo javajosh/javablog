94db0127f20a3da2781f90a8bce71ccccb863531
javajosh <javajosh@gmail.com>
2025-03-05 00:18:33 -0500

    Fixing formatting on the logger; adding support for a pre-commit hook to check formatting before commit.

--- src/main/java/com/javajosh/javablog/RequestLogger.java
+++ src/main/java/com/javajosh/javablog/RequestLogger.java
@@ -2,27 +2,25 @@ package com.javajosh.javablog;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
-
+import org.springframework.stereotype.Component;
 
 @Component
 public class RequestLogger implements Filter {
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
     HttpServletRequest httpRequest = (HttpServletRequest) request;
 
-    String requestLog = String.format("Method: %s, Path: %s, Client IP: %s",
-        httpRequest.getMethod(),
-        httpRequest.getRequestURI(),
-        httpRequest.getRemoteAddr());
+    String requestLog =
+        String.format(
+            "Method: %s, Path: %s, Client IP: %s",
+            httpRequest.getMethod(), httpRequest.getRequestURI(), httpRequest.getRemoteAddr());
 
     System.out.println("Incoming Request: " + requestLog);
 
