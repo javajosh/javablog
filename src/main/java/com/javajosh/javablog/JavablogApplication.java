3daabe9384b3fcd0a72e0dcc5a2f704279d866c3
javajosh <javajosh@gmail.com>
2025-03-06 00:31:54 -0500

    java: demonstrate running arbitrary code at startup

--- src/main/java/com/javajosh/javablog/JavablogApplication.java
+++ src/main/java/com/javajosh/javablog/JavablogApplication.java
@@ -1,12 +1,17 @@
 package com.javajosh.javablog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavablogApplication {
 
+  private static final Logger log = LoggerFactory.getLogger(JavablogApplication.class);
+
   public static void main(String[] args) {
+    log.info("JavablogApplication started");
     SpringApplication.run(JavablogApplication.class, args);
   }
 }
